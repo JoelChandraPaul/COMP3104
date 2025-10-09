f6c7d77f4d079f8b36fc7064883d220ebdd078db
Joel Chandra Paul <joelcpaul02@gmail.com>
2025-10-09 14:48:43 -0400

    Made changes to app.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101512597 || Joel Chandra Paul || DevOps</h5>
+      <h5>Developed By: 101512597 || Joel Chandra Paul || COMP3104 || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
